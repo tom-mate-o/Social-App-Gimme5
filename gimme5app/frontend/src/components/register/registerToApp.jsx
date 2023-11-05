6a57e0ab861d29ff2dba50484c05634ec0b42f89
k-huber <141048452+k-huber@users.noreply.github.com>
2023-11-05 15:40:55 +0100

    add and change register form styling

--- gimme5app/frontend/src/components/register/registerToApp.jsx
+++ gimme5app/frontend/src/components/register/registerToApp.jsx
@@ -14,7 +14,7 @@ export default function RegisterToApp() {
     <div className="register">
       <h1>REGISTER</h1>
 
-      <form>
+      <form className="register-form">
         <input style={{ display: "none" }} type="file" id="file" name="file" />
         <label
           htmlFor="file"
@@ -27,64 +27,57 @@ export default function RegisterToApp() {
         >
           Select a Profile Picture
         </label>
+
+        <input ref={username} id="username" type="text" placeholder="Username*" required />
+
+        <input
+          ref={firstname} id="firstname" type="text" placeholder="First Name*" required />
+        <input ref={lastname} id="lastname" type="text" placeholder="Last Name" required />
+
+        <input
+          ref={password}
+          id="password"
+          type="password"
+          placeholder="Password*"
+          required
+        />
+
+        <input
+          ref={password2}
+          id="password2"
+          type="password"
+          placeholder="Repeat Password*"
+          required
+        />
+
+        <input ref={email} id="email" type="email" placeholder="Email*" required />
+        <input ref={email2} id="email2" type="email" placeholder="Repeat Email*" required />
+
+        <input ref={location} id="location" type="text" placeholder="Location" />
+
+        <button
+          onClick={() => {
+            console.log(username.current.value);
+            console.log(firstname.current.value);
+            console.log(lastname.current.value);
+            console.log(password.current.value);
+            console.log(password2.current.value);
+            console.log(email.current.value);
+            console.log(email2.current.value);
+            console.log(location.current.value);
+            username.current.value = "";
+            firstname.current.value = "";
+            lastname.current.value = "";
+            password.current.value = "";
+            password2.current.value = "";
+            email.current.value = "";
+            email2.current.value = "";
+            location.current.value = "";
+          }}
+        >
+          Register
+        </button>
       </form>
-
-      <input ref={username} id="username" type="text" placeholder="Username*" />
-
-      <input
-        ref={firstname}
-        id="firstname"
-        type="text"
-        placeholder="First Name*"
-      />
-      <input ref={lastname} id="lastname" type="text" placeholder="Last Name" />
-
-      <input
-        ref={password}
-        id="password"
-        type="password"
-        placeholder="Password*"
-      />
-
-      <input
-        ref={password2}
-        id="password2"
-        type="password"
-        placeholder="Repeat Password*"
-      />
-
-      <input ref={email} id="email" type="email" placeholder="Email*" />
-      <input
-        ref={email2}
-        id="email2"
-        type="email"
-        placeholder="Repeat Email*"
-      />
-
-      <input ref={location} id="location" type="text" placeholder="Location" />
-
-      <button
-        onClick={() => {
-          console.log(username.current.value);
-          console.log(firstname.current.value);
-          console.log(lastname.current.value);
-          console.log(password.current.value);
-          console.log(password2.current.value);
-          console.log(email.current.value);
-          console.log(email2.current.value);
-          console.log(location.current.value);
-          username.current.value = "";
-          firstname.current.value = "";
-          lastname.current.value = "";
-          password.current.value = "";
-          password2.current.value = "";
-          email.current.value = "";
-          email2.current.value = "";
-          location.current.value = "";
-        }}
-      >
-        Register
-      </button>
 
       <p>*Pflichtfeld</p>
     </div>
