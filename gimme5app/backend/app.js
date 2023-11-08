1e85feb6559ae8db5741797b8a19f6e0b491c5cf
tom-mate-o <132936694+tom-mate-o@users.noreply.github.com>
2023-11-08 09:01:59 +0100

    update main (forgot to create a new branch)

--- gimme5app/backend/app.js
+++ gimme5app/backend/app.js
@@ -10,62 +10,75 @@ const cors = require('cors');
 app.use(express.json());
 app.use(cors());
 
-// MongoDB verbinden
-const connectString = process.env.MONGO_DB_CLIENT;
 
-console.log("Connecting to MongoDB...");
+// Connect to MongoDB
+    const connection = mongoose.createConnection(process.env.MONGO_DB_CLIENT, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    connection.once("error", () => {
+      console.log("Error connecting to MongoDB");
+    });
+    connection.once("open", () => {
+      console.log("Connected to MongoDB");
+    });
 
-mongoose.connect(connectString, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-}).then(() => {
-  console.log("Connected to MongoDB");
-}).catch((error) => {
-  console.error("Failed to connect to MongoDB:", error);
-});
 
-// ---- ursprüngliche Version mit async/await aus der todo-app, funktioniert hier nicht ----
+    // Schema
+  const topFiveSchema = new mongoose.Schema({
+    id: String,
+    user: String,
+    connection: String,
+    subcategory: String,
+    list: [
+      {
+        place1: String,
+        place2: String,
+        place3: String,
+        place4: String,
+        place5: String,
+      },
+    ],
+    hastags: [String],
+    likes: Number,
+    postprivate: Boolean,
+  });
 
-// app.use(async (req, res, next) => { 
-//   console.log("Connecting to MongoDB...");
-//   try {
-//     const connection = await mongoose.createConnection(connectString, {
-//       useNewUrlParser: true,
-//       useUnifiedTopology: true,
-//     });
-//     console.log("Connected to MongoDB");
-//     req.db = connection;
-//     next();
-//   } catch (error) {
-//     console.error("Failed to connect to MongoDB:", error);
-//     next(error);
+  const TopFive = connection.model("TopFiveCollection", topFiveSchema); // TopFiveCollection ist der Namer der Collection in der DB
+
+  // Methode um TopFive Liste in die DB zu schreiben
+// app.post("/addtopfive", (req, res) => {
+//   try{
+//     ...
 //   }
-// });
-
+  
 
 // Routen
 app.get('/', (req, res) => {
   res.send('Huhu! app.js is here');
 });
 
+
 app.get('/db-status', (req, res) => {
   let state = 'Unknown';
-  switch(mongoose.connection.readyState) {
+  switch (connection.readyState) {
     case 0:
-      state = 'disconnected';
+      state = 'Disconnected';
       break;
     case 1:
-      state = 'connected';
+      state = 'Connected';
       break;
     case 2:
-      state = 'connecting';
+      state = 'Connecting';
       break;
     case 3:
-      state = 'disconnecting';
+      state = 'Disconnecting';
       break;
   }
   res.send(`Database connection state: ${state}`);
+ 
 });
+
 
 app.get("/health-check", (req, res) => {
   res.status(200).send({message: "I'm alive. Greetings from the backend."});
