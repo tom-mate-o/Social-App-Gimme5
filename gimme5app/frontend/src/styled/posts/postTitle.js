99a08b72e90131138f089d7cedc1c2365d99bfd1
k-huber <141048452+k-huber@users.noreply.github.com>
2023-11-13 17:53:37 +0100

    add font weight bold to title

--- gimme5app/frontend/src/styled/posts/postTitle.js
+++ gimme5app/frontend/src/styled/posts/postTitle.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 export const PostTitle = styled.div`
     font-size: 20px;
+    font-weight: bold;
     text-transform: uppercase;
     margin-bottom: 5px;
     `;
